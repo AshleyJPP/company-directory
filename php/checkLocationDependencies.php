ebdc9e9cf8a6ab6fed889867c5fa9965a36e79e6
AshleyJPP <139067880+AshleyJPP@users.noreply.github.com>
2023-11-02 23:41:53 +0000

    Update checkLocationDependencies.php

--- php/checkLocationDependencies.php
+++ php/checkLocationDependencies.php
@@ -2,25 +2,29 @@
 include 'config.php';
 
 $locationId = $_POST['locationId'];
-
 $response = array();
 
 try {
-    $query = "SELECT COUNT(*) as departmentCount FROM department WHERE locationID = ?";
+    $query = "SELECT COUNT(d.id) as departmentCount, l.name as locationName 
+              FROM location l
+              LEFT JOIN department d ON d.locationID = l.id 
+              WHERE l.id = ?";
     $stmt = $conn->prepare($query);
     $stmt->bind_param("i", $locationId);
     $stmt->execute();
     $result = $stmt->get_result();
     $row = $result->fetch_assoc();
-    $response['departmentCount'] = $row['departmentCount'];
 
-if ($row['departmentCount'] > 0) {
-    $response['status'] = 'error';
-    $response['message'] = 'You cannot remove this location as it has ' . $row['departmentCount'] . ' departments assigned to it.';
-} else {
-    $response['status'] = 'success';
-    $response['message'] = 'No departments associated with this location. Safe to delete.';
-}
+    $response['departmentCount'] = $row['departmentCount'];
+    $response['locationName'] = $row['locationName'];
+
+    if ($row['departmentCount'] > 0) {
+        $response['status'] = 'error';
+        $response['message'] = 'You cannot remove ' . $row['locationName'] . ' as it has ' . $row['departmentCount'] . ' departments assigned to it.';
+    } else {
+        $response['status'] = 'success';
+        $response['message'] = 'No departments associated with ' . $row['locationName'] . '. Safe to delete.';
+    }
 
 } catch(Exception $e) {
     $response['status'] = 'error';
